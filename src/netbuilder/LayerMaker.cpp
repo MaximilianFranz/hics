7bac0cc47b490c959db472f718fb79f44edb9c15
David Culley <david.culley@student.kit.edu>
2018-01-08 04:53:57 +0100

    implement LayerMaker

--- src/netbuilder/LayerMaker.cpp
+++ src/netbuilder/LayerMaker.cpp
@@ -1,5 +1,13 @@
 //
-// Created by Maximilian Franz on 07.01.18.
+// Created by David Culley on 07.01.18.
 //
 
 #include "LayerMaker.h"
+
+void LayerMaker::createInputLayer(int inputDimension) {
+
+}
+
+void LayerMaker::createConvLayer(int inputDimension) {
+
+}
