9c58773e8dd30c87cdf89b829d4085f2fa3a1801
David Culley <david.culley@student.kit.edu>
2018-01-08 23:39:12 +0100

    remove non-working auto-generated code

--- src/netbuilder/wrapper/Wrapper.h
+++ src/netbuilder/wrapper/Wrapper.h
@@ -10,11 +10,9 @@ class Wrapper {
 protected:
     int numDimensions;
     int dimensionSizes;
-//    std::vector data{};
+//    std::vector data;
 public:
     // must have pure virtual functions
-//    Wrapper(int numDimensions, int dimensionSizes, const std::vector &data)
-//            : data(data), dimensionSizes(dimensionSizes), numDimensions(numDimensions) {}
 
     virtual int getNumDimensions() const {
         return numDimensions;
@@ -24,7 +22,4 @@ public:
         return dimensionSizes;
     }
 
-//    virtual const std::vector &getData() const {
-//        return data;
-//    }
 };
