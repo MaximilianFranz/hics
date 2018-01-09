b545beecdebd71c37c8e3e0091eafd7e23e8983d
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-09 14:44:37 +0100

    add method bodies

--- src/manager/PreProcessor.cpp
+++ src/manager/PreProcessor.cpp
@@ -3,3 +3,8 @@
 //
 
 #include "PreProcessor.h"
+
+void PreProcessor::setOutputSize(int w, int h) {
+    width = w;
+    height = h;
+}
--- src/manager/PreProcessor.h
+++ src/manager/PreProcessor.h
@@ -6,8 +6,31 @@
 #define HICS_PREPROCESSOR_H
 
 
-class PreProcessor {
+#include <wrapper/ImageWrapper.h>
+#include <map>
+#include <QString>
+#include <QImage>
 
+class PreProcessor {
+private:
+    //Output dimensions needed by the NeuralNet
+    int width;
+    int height;
+
+public:
+    /**
+     * Sets the output size
+     * @param w
+     * @param h
+     */
+    void setOutputSize(int w, int h);
+
+    /**
+     * Processes the input images from QImages to ImageWrappers that the System uses.
+     * @param imgMap
+     * @return
+     */
+    std::vector<ImageWrapper> processImages(std::map<QImage, QString> imgMap);
 };
 
 
