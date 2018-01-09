758a3de84059fdc219f7c7af4282157f31d63f26
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-09 13:27:42 +0100

    add method bodies

--- src/manager/PerformanceData.h
+++ src/manager/PerformanceData.h
@@ -6,8 +6,21 @@
 #define HICS_PERFORMANCEDATA_H
 
 
-class PerformanceData {
+#include <PlatformInfo.h>
+#include <map>
 
+class PerformanceData {
+private:
+    int PowerConsumption;       //saved in ws (watt seconds)
+    int ComputationTime;        //saved in ms
+    std::map<PlatformInfo, float> platformUsage;    //maps each platform to a percentage of usage
+
+public:
+    int getPowerConsumption() const;
+
+    int getComputationTime() const;
+
+    const std::map<PlatformInfo, float> &getPlatformUsage() const;
 };
 
 
