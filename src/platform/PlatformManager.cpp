745312eb1b9b28621d3ba43565f610e1eab0ae9d
Michael Biebl <mbiebl@gmail.com>
2018-01-10 18:07:44 +0100

    Implement Singleton functionality of PlatformManager

--- src/platform/PlatformManager.cpp
+++ src/platform/PlatformManager.cpp
@@ -2,4 +2,23 @@
 // Created by michael on 07.01.18.
 //
 
+#include <iostream>
+#include <vector>
+
+#include "platforms/Platform.h"
 #include "PlatformManager.h"
+
+std::vector<Platform> PlatformManager::getPlatforms() {
+    return std::vector<Platform>();
+}
+
+PlatformManager& PlatformManager::getInstance() {
+    static PlatformManager instance;
+
+    return instance;
+}
+
+std::vector<PlatformInfo> PlatformManager::getPlatformInfos() {
+    return std::vector<PlatformInfo>();
+}
+
--- src/platform/PlatformManager.h
+++ src/platform/PlatformManager.h
@@ -4,7 +4,24 @@
 
 #pragma once
 
+#include <vector>
+
+#include "platforms/Platform.h"
+#include "PlatformInfo.h"
 
 class PlatformManager {
+private:
+    // Private constructor
+    PlatformManager() {};
 
+public:
+    // https://stackoverflow.com/questions/1008019/c-singleton-design-pattern
+    PlatformManager(PlatformManager const&) = delete;
+    PlatformManager& operator=(PlatformManager const &) = delete;
+
+    static PlatformManager& getInstance();
+
+    std::vector<Platform> getPlatforms();
+
+    std::vector<PlatformInfo> getPlatformInfos();
 };
