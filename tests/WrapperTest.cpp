631580eeacb6c37706b3f2ce413ef4cc11fac288
Max <franzmaximilian1996@gmail.com>
2018-01-10 14:37:20 +0100

    adapted test cases to new structure

--- tests/WrapperTest.cpp
+++ tests/WrapperTest.cpp
@@ -8,9 +8,10 @@
 TEST_CASE("Return functions of Wrapper", "[sometag]") {
     std::vector<float> data(5,1.0);
     std::vector<int> s(1,5);
-    DataWrapper d(1, s, data);
+    DataWrapper d(s, data);
+    std::string examplepath = "src/test/somefile.jpg";
+
     REQUIRE(d.getNumDimensions() == 1);
-    std::vector<int> sizes = d.getDimensionSizes();
-    REQUIRE(sizes[0] == 5);
+    REQUIRE(d.getDimensionSizes()[0] == 5);
     REQUIRE(d.getSizeOfDimension(1) == 5);
 }
