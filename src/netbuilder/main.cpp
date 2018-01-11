33644b95941d0f6a23e464052e7e02b1793e1633
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-11 23:35:57 +0100

    Added little test program to the main in netbuilder.

--- src/netbuilder/main.cpp
+++ src/netbuilder/main.cpp
@@ -1,4 +1,4 @@
-#include "loader/weightloader/AlexNetWeightLoader.h"
+#include <loader/weightloader/AlexNetWeightLoader.h>
 
 int main(void){
 
@@ -6,5 +6,15 @@ int main(void){
     FILE_PATH = "/home/pselab/Dokumente/repo/hics/src/netbuilder/loader/weightloader/alexnet_weights.h5";
 
     AlexNetWeightLoader alex(FILE_PATH);
+
+    WeightWrapper test = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::FULLY_CON_1);
+
+    for(int i = 0; i<4096; i++){
+
+        std::cout << "Bias index " << i << " has value : ";
+        std::cout << test.getBias().at(i) << std::endl;
+
+    }
+
     return 0;
 }
