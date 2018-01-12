65767f3ce53c61b50604b2688585b6fa57562d17
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-12 16:48:49 +0100

    Removed unnecessary CMake commands and resetted netbuilder main.cpp.

--- src/netbuilder/main.cpp
+++ src/netbuilder/main.cpp
@@ -2,13 +2,13 @@
 
 int main(void){
 
-    std::string FILE_PATH;
+    /*std::string FILE_PATH;
     FILE_PATH = "/home/pselab/Dokumente/repo/hics/src/netbuilder/loader/weightloader/alexnet_weights.h5";
 
     AlexNetWeightLoader alex(FILE_PATH);
 
     WeightWrapper test = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_2);
-
+*/
 
     return 0;
 }
