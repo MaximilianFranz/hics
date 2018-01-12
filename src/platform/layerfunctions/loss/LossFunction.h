bc5d6f79665b7daffb97c73edf753fc626f8af81
David Culley <david.culley@student.kit.edu>
2018-01-12 16:56:08 +0100

    create LossFunction

---
+++ src/platform/layerfunctions/loss/LossFunction.h
@@ -0,0 +1,15 @@
+//
+// Created by David Culley on 12.01.18.
+//
+
+#pragma once
+
+
+#include <layerfunctions/LayerFunction.h>
+
+class LossFunction : public LayerFunction {
+public:
+    void execute();
+};
+
+
