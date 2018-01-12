6be51d92fc9e0335a6a588421e2f728327bd1bac
David Culley <david.culley@student.kit.edu>
2018-01-12 16:46:57 +0100

    create ConvolutionFunction

---
+++ src/platform/layerfunctions/convolution/ConvolutionFunction.cpp
@@ -0,0 +1,10 @@
+//
+// Created by David Culley on 12.01.18.
+//
+
+#include <NotImplementedException.h>
+#include "ConvolutionFunction.h"
+
+void ConvolutionFunction::execute() {
+    throw NotImplementedException();
+}
