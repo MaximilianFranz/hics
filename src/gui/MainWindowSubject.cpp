97f6fe4e74260089eda7bb987e192d223e3af7c9
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-16 19:19:45 +0100

    Implemented MainWindowSubject's methods and removed its source file so its header only.

--- src/gui/MainWindowSubject.cpp
+++
@@ -1,7 +0,0 @@
-#include "MainWindowSubject.h"
-
-MainWindowSubject::MainWindowSubject()
-{
-
-}
-
--- src/gui/MainWindowSubject.h
+++ src/gui/MainWindowSubject.h
@@ -1,8 +1,32 @@
 #pragma once
 
+#include <list>
+#include "ManagerObserver.h"
 
-class MainWindowSubject
-{
+class MainWindowSubject {
+
+private:
+
+    std::list<ManagerObserver> observers;
+
 public:
+
+    void attach(const ManagerObserver &manager) {
+        observers.push_back(manager);
+    }
+
+    void detach(const ManagerObserver &manager){
+        observers.remove(manager);
+    }
+
+    void notify(){
+        std::list<ManagerObserver>::iterator it;
+
+        for(it = observers.begin(); it != observers.end(); ++it){
+            it->update();
+        }
+    }
+
+    //TODO Needed?
     MainWindowSubject();
 };
