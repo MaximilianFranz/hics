a83b117de8d3cb5375fad066007e8097e9bd6ad6
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-16 12:45:30 +0100

    create tests for PreProcessor
    Implement different testcases for PreProcessor and make minor fixes in the PreProcessor itself.

--- src/manager/PreProcessor.cpp
+++ src/manager/PreProcessor.cpp
@@ -31,20 +31,27 @@ std::vector<ImageWrapper> PreProcessor::processImages(std::map<QString, QImage> 
             for (int y = 0; y < height; ++y) {
                 for (int x = 0; x < width; ++x) {
                     float currentPixel;
-                    switch (rgb) {
-                        case 0:
-                            currentPixel = currentImg.pixelColor(x, y).red();
-                            break;
-                        case 1:
-                            currentPixel = currentImg.pixelColor(x, y).green();
-                            break;
-                        case 2:
-                            currentPixel = currentImg.pixelColor(x, y).blue();
-                            break;
-                        default:
-                            //TODO: specify exception
-                            throw std::exception();
+                    //Fill image with black pixels if dimension is wrong
+                    if (x >= currentImg.width() || y >= currentImg.height()) {
+                        currentPixel = 0;
+                        bitmap.push_back(currentPixel);
+                        continue;
                     }
+                        //extract the right RGB channel
+                        switch (rgb) {
+                            case 0:
+                                currentPixel = currentImg.pixelColor(x, y).red();
+                                break;
+                            case 1:
+                                currentPixel = currentImg.pixelColor(x, y).green();
+                                break;
+                            case 2:
+                                currentPixel = currentImg.pixelColor(x, y).blue();
+                                break;
+                            default:
+                                //TODO: specify exception
+                                throw std::exception();
+                        }
                     bitmap.push_back(currentPixel);
                 }
             }
