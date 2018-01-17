47b33f1f4ce529471a40fa65b75117ca853b791b
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-17 19:21:48 +0100

    Overloaded comparison operator in ManagerObserver.

--- src/manager/ManagerObserver.h
+++ src/manager/ManagerObserver.h
@@ -8,8 +8,21 @@
  * This interface is implemented by the manager to observe the GUI. It follows the observer-pattern structure.
  */
 class ManagerObserver {
+
+public:
+
     /**
      * Observer method for the manager to implement
      */
     virtual void update();
+
+    /**
+     * @brief Overloads operator==() function to compare two ManagerObserver's on their identity.
+     *
+     * @param managerObserver the to be compared ManagerObserver
+     * @return true if they are equal, false if not.
+     */
+    bool operator==(const ManagerObserver &managerObserver){
+        return this == &managerObserver;
+    }
 };
