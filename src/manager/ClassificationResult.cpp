c0eaf454d34125d46aa646ff8b2c9583639d4f89
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-18 21:06:51 +0100

    Implementation of aggregateResults()
    Changed aggregatedResult attribute to match ImageWrapper. Testing still pending.

--- src/manager/ClassificationResult.cpp
+++ src/manager/ClassificationResult.cpp
@@ -2,6 +2,8 @@
 // Created by jallmenroeder on 07/01/18.
 //
 
+#include <algorithm>
+#include <functional>
 #include "ClassificationResult.h"
 
 ClassificationResult::ClassificationResult(const std::vector<ImageResult> &results, const NetInfo &usedNet,
@@ -20,10 +22,44 @@ const PerformanceData &ClassificationResult::getPerformance() const {
     return performance;
 }
 
-const std::map<std::string, float> &ClassificationResult::getAggregatedResults() const {
+const std::vector<std::pair<std::string, float>> &ClassificationResult::getAggregatedResults() const {
     return aggregatedResult;
 }
 
-void ClassificationResult::aggregateResults() {
-    //TODO: Implementation of aggregateResults
+
+bool isEqual(const std::pair<std::string, float>& element1, const std::pair<std::string, float>& element2) {
+    return element1.first == element2.first;
 }
+
+void ClassificationResult::aggregateResults() {
+
+    for (auto const &imageIt : results) {
+
+        for (auto resultIt : imageIt.getResults()) {
+
+            //MAGIC  checks if a label is already added to the vector
+            auto currentElement = std::find_if(aggregatedResult.begin(), aggregatedResult.end(),
+                                               [](std::pair<std::string, float>& element){
+                                                   return element.first == "k";
+                                               });
+
+            //insert label if new, sum up probability otherwise
+            if (currentElement == aggregatedResult.end()) {
+
+                aggregatedResult.push_back(resultIt);
+            } else {
+                currentElement.operator*().second += resultIt.second;
+            }
+
+        }
+    }
+
+    for (auto aggIt : aggregatedResult) {
+        //TODO: check if size() works
+        aggIt.second = aggIt.second/aggregatedResult.size();
+    }
+
+    aggregatedResult.erase(aggregatedResult.begin() + 5, aggregatedResult.end());
+}
+
+
