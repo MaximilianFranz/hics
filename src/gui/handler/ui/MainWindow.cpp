9b04449353125b56b6c9e00954ff83c37c903c9e
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-18 12:10:13 +0100

    Added comments to MainWindow and deleted all signals in it.

--- src/gui/handler/ui/MainWindow.cpp
+++ src/gui/handler/ui/MainWindow.cpp
@@ -6,12 +6,17 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->centralwidget(mainWindowQStackedWidget); //TODO check if this is enough to full screen mainWindowQStackedWidget
+}
 
+void MainWindow::addWidgetToStack(QWidget &widget){
+    mainWindowQStackedWidget.addWidget(&widget);
 }
 
 void MainWindow::setCurrentWidget(QWidget &widget){
     int index = mainWindowQStackedWidget.indexOf(&widget);
 
+    //Check if widget is inside mainWindowQStackedWidget
     if(index >= 0){
         mainWindowQStackedWidget.setCurrentWidget(&widget);
     }
--- src/gui/handler/ui/MainWindow.h
+++ src/gui/handler/ui/MainWindow.h
@@ -7,6 +7,16 @@ namespace Ui {
 class MainWindow;
 }
 
+/**
+ * @brief The MainWindow class is the main window of the GUI. It's current widget can be changed so that custom widgets
+ *        can be displayed.
+ *
+ * The MainWindow class inherits QMainWindow. It represents the main window of the GUI and consists of the
+ * mainWindowQStackedWidget which can display one QWidget at a time. MainWindow provides a public slot to add and change
+ * the current widget. MainWindowHandler is responsible for handling it.
+ *
+ * @author Patrick Deubel
+ */
 class MainWindow : public QMainWindow {
 
     Q_OBJECT
@@ -18,11 +28,18 @@ public:
 
 public slots:
 
+    /**
+     * @brief addWidgetToStack adds the given widget to mainWindowQStackedWidget.
+     * @param widget is the to be added widget
+     */
+    void addWidgetToStack(QWidget &widget);
+
+    /**
+     * @brief setCurrentWidget sets the given widget to the currently displayed one in mainWindowQStackedWidget, if it
+     *        is stored in it.
+     * @param widget is the to be displayed widget.
+     */
     void setCurrentWidget(QWidget &widget);
-
-signals:
-
-    void test();
 
 private:
     Ui::MainWindow *ui;
