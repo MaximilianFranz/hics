d97d1202e52dc04e3f5eaab2fc68290d77fffc5e
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-18 12:38:35 +0100

    Added definitions of some more public functions and slots.

--- src/gui/handler/StartWidgetHandler.h
+++ src/gui/handler/StartWidgetHandler.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <QObject>
 #include <list>
 #include <NetInfo.h>
 #include <PlatformInfo.h>
 #include <OperationMode.h>
 
-class StartWidgetHandler
-{
+class StartWidgetHandler : public QObject {
+
+    Q_OBJECT
+
 public:
 
     /**
@@ -27,31 +30,65 @@ public:
     StartWidgetHandler(std::list<NetInfo> &neuralNets, std::list<PlatformInfo> &platforms,
                        std::list<OperationMode> &operationModes);
 
+    /**
+     * @brief getSelectedNeuralNet
+     * @return
+     *
+     * This method gets the selected neural net from startWidget and returns it.
+    @return: The selected neural net.
+     */
+    NetInfo getSelectedNeuralNet();
+
+    /**
+     * @brief getSelectedPlatforms
+     * @return
+     * This method gets the selected platforms from startWidget and returns it.
+@return: The selected platforms.
+     */
+    std::list<PlatformInfo> getSelectedPlatforms();
+
+    /**
+     * @brief getSelectedOperationMode
+     * @return
+     * This method gets the selected operation mode from startWidget and returns it.
+@return: The selected operation mode.
+     */
+    OperationMode getSelectedOperationMode();
+
+public slots:
+
+    /**
+     * @brief processInputImageButton
+     * This method is the slot function to the signal click() on the selectInputImageQPushButton
+    from the startWidget. It opens a file chooser where the user can select between one
+    and twenty images. The selected images then get stored and displayed together with
+    their file path.
+     */
+    void processInputImageButton();
+
+    /**
+     * @brief processConfirmDeletionButton
+     * This method is the slot function to the click signal on the confirmDeletionQPushButton,
+     * which will remove all images the user selected for deletion.
+     */
+    void processConfirmDeletionButton();
+
+    /**
+     * @brief processAbortDeletionButton
+     * This method is the slot function to the signal click() on the abortDeletionQPushButton.
+It will abort the deletion of user images by unchecking all deletion check boxes.
+23
+     */
+    void processAbortDeletionButton();
 
 };
 
 /*
-◦ + processInputImageButton() : void
-This method is the slot function to the signal click() on the selectInputImageQPushButton
-from the startWidget. It opens a file chooser where the user can select between one
-and twenty images. The selected images then get stored and displayed together with
-their file path.
-◦ + processConfirmDeletionButton() : void
-This method is the slot function to the click signal on the confirmDeletionQPushButton,
-which will remove all images the user selected for deletion.
-◦ + processAbortDeletionButton() : void
-This method is the slot function to the signal click() on the abortDeletionQPushButton.
-It will abort the deletion of user images by unchecking all deletion check boxes.
-23
-◦ + getSelectedNeuralNet() : NetInfo
-This method gets the selected neural net from startWidget and returns it.
-@return: The selected neural net.
-◦ + getSelectedPlatforms() : list<PlatformInfo>
-This method gets the selected platforms from startWidget and returns it.
-@return: The selected platforms.
-◦ + getSelectedOperationMode() : OperationMode
-This method gets the selected operation mode from startWidget and returns it.
-@return: The selected operation mode.
+
+
+◦
+
+
 ◦ + isAggregated() : bool
 This method looks up in startWidget if the user selected to aggregate the results.
 @return: true, if the results should be aggregated, false if not.
