29b2aa5e26171302b953424fd61093c4f34f65c6
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-19 13:18:40 +0100

    create class bodies for testclasses

---
+++ tests/manager/ClassificationResultTest.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jallmenroeder on 19/01/18.
+//
+
+#include "ClassificationResultTest.h"
+
+SCENARIO("aggregateResult for trivial test case") {
+    
+}
---
+++ tests/manager/ClassificationResultTest.h
@@ -0,0 +1,7 @@
+//
+// Created by jallmenroeder on 19/01/18.
+//
+
+#pragma once
+
+#include "catch.hpp"
