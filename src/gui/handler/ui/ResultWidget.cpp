e37007fc119967c8775ddad397aeab22e1617c9b
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-22 22:03:08 +0100

    Added user interface to ResultWidget and declared its methods.

--- src/gui/handler/ui/ResultWidget.cpp
+++ src/gui/handler/ui/ResultWidget.cpp
@@ -1,5 +1,6 @@
 #include "handler/ui/ResultWidget.h"
 #include "ui_ResultWidget.h"
+#include "../NotImplementedException.h"
 
 ResultWidget::ResultWidget(QWidget *parent) :
     QWidget(parent),
@@ -11,4 +12,16 @@ ResultWidget::ResultWidget(QWidget *parent) :
 ResultWidget::~ResultWidget()
 {
     delete ui;
+}
+
+void ResultWidget::displayResults(ClassificationResult &classificationResult){
+    throw NotImplementedException();
+}
+
+QPushButton* ResultWidget::getDetailsQPushButton(){
+    return ui->detailsQPushButton;
+}
+
+QPushButton* ResultWidget::getReturnQPushButton(){
+    return ui->returnQPushButton;
 }
--- src/gui/handler/ui/ResultWidget.h
+++ src/gui/handler/ui/ResultWidget.h
@@ -1,19 +1,30 @@
 #pragma once
 
 #include <QWidget>
+#include <QPushButton>
+#include "ClassificationResult.h"
 
 namespace Ui {
 class ResultWidget;
 }
 
-class ResultWidget : public QWidget
-{
+class ResultWidget : public QWidget {
+
     Q_OBJECT
 
 public:
+
     explicit ResultWidget(QWidget *parent = 0);
+
     ~ResultWidget();
 
+    void displayResults(ClassificationResult &classificationResult);
+
+    QPushButton* getDetailsQPushButton();
+
+    QPushButton* getReturnQPushButton();
+
 private:
+
     Ui::ResultWidget *ui;
 };
