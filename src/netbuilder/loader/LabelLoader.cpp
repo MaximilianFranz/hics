f08049c808acffebfecff1b58dd88ae057f81ef8
Max <franzmaximilian1996@gmail.com>
2018-01-22 21:31:34 +0100

    implemented simple LabelLoader

--- src/netbuilder/loader/LabelLoader.cpp
+++ src/netbuilder/loader/LabelLoader.cpp
@@ -2,4 +2,27 @@
 // Created by Maximilian Franz on 22.01.18.
 //
 
+#include <fstream>
 #include "LabelLoader.h"
+
+std::map<int, std::string> LabelLoader::getLabelMap(std::string path) {
+
+    std::map<int, std::string> labelMap;
+
+    char resolved_path[1024];
+    realpath(path.c_str(), resolved_path);
+    std::ifstream file(resolved_path);
+    std::string str;
+
+    std::string line;
+
+    int index = 0;
+
+    while (std::getline(file, line)) {
+        labelMap.emplace(std::pair<int, std::string>(index, line));
+        index++;
+    }
+
+    return labelMap;
+
+}
