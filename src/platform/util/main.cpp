889a7e4ce259102e1d686f16d9a7bfc12f1f673f
David Culley <david.culley@student.kit.edu>
2018-01-25 18:14:13 +0100

    write simple test cases

--- src/platform/util/main.cpp
+++ src/platform/util/main.cpp
@@ -4,7 +4,7 @@
 #include <util/im2col.h>
 
 int main(int argc, char *argv[]) {
-    int image_size = 3;
+    int image_size = 4;
     int kernel_size = 2;
     int channels = 1;
     int padding = 0;
@@ -14,13 +14,46 @@ int main(int argc, char *argv[]) {
     std::cout << "Image size: " << image_size << "x" << image_size << std::endl;
     std::cout << "Kernel size: " << kernel_size << "x" << kernel_size << std::endl;
     std::cout << "Padding: " << padding << std::endl;
-    std::cout << "Output size: " << output_size << "x" << output_size << std::endl;
 
-    auto image = std::vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    auto column = std::vector<float>();
+    std::cout << std::endl;
+    std::cout << "Ergebnismatrix M:" << std::endl;
+    std::cout << "Anzahl der Spalten: " << kernel_size * kernel_size << std::endl;
+    std::cout << "Anzahl der Zeilen: " << output_size * output_size << std::endl;
+    std::cout << std::endl;
 
-    helper::im2col_cpu(image.data(), channels, image_size, image_size, kernel_size, padding, stride,
-                       column.data());
+    auto column = std::vector<float>(
+            static_cast<unsigned long>(kernel_size * kernel_size * channels * output_size * output_size));
+
+    auto image = std::vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+    std::cout << "Image: " << std::endl;
+    int counter = 0;
+    for (std::vector<float>::const_iterator i = image.begin(); i != image.end(); ++i) {
+        if (counter >= image_size) {
+            std::cout << std::endl;
+            counter = 0;
+        }
+        std::cout << *i << ' ';
+        counter++;
+    }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    helper::im2col_cpu(image.data(), channels, image_size, image_size, kernel_size, padding, stride, column.data());
+
+    std::cout << "Ergebnismatrix: " << std::endl;
+    counter = 0;
+    for (std::vector<float>::const_iterator i = column.begin(); i != column.end(); ++i) {
+
+        if (counter >= kernel_size * kernel_size * channels) {
+            std::cout << std::endl;
+            counter = 0;
+        }
+
+        std::cout << *i << ' ';
+        counter++;
+    }
 
     return 0;
 }
--- tests/platform/util/im2colTest.cpp
+++ tests/platform/util/im2colTest.cpp
@@ -1,3 +1,48 @@
 #include "im2colTest.h"
 
 #include <iostream>
+#include <util/im2col.h>
+
+TEST_CASE("3x3x1 image and 3x3x1 kernel") {
+    int image_size = 3;
+    int kernel_size = 3;
+    int channels = 1;
+    int padding = 0;
+    int stride = 1;
+    int output_size = (image_size - kernel_size + 2 * padding) / stride + 1;
+    auto image = std::vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    auto column = std::vector<float>(
+            static_cast<unsigned long>(kernel_size * kernel_size * channels * output_size * output_size));
+
+    helper::im2col_cpu(image.data(), channels, image_size, image_size, kernel_size, padding, stride, column.data());
+
+    REQUIRE(column == std::vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9});
+}
+
+TEST_CASE("3x3x1 image and 2x2x1 kernel") {
+    int image_size = 3;
+    int kernel_size = 2;
+    int channels = 1;
+    int padding = 0;
+    int stride = 1;
+    int output_size = (image_size - kernel_size + 2 * padding) / stride + 1;
+
+    auto column = std::vector<float>(
+            static_cast<unsigned long>(kernel_size * kernel_size * channels * output_size * output_size));
+
+    SECTION("") {
+        auto image = std::vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        helper::im2col_cpu(image.data(), channels, image_size, image_size, kernel_size, padding, stride, column.data());
+
+        REQUIRE(column == std::vector<float>{1, 2, 4, 5, 2, 3, 5, 6, 4, 5, 7, 8, 5, 6, 8, 9});
+    }
+
+    SECTION("") {
+        auto image = std::vector<float>{5, 1, 0, 2, 9, 5, 8, 1, 4};
+
+        helper::im2col_cpu(image.data(), channels, image_size, image_size, kernel_size, padding, stride, column.data());
+
+        REQUIRE(column == std::vector<float>{5, 1, 2, 9, 1, 0, 9, 5, 2, 9, 8, 1, 9, 5, 1, 4});
+    }
+}
