5137bd193cc9eed28a2da4d0fe69c36d3060765c
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-25 10:46:24 +0100

    Added class comments to ResultWidget.

--- src/gui/handler/ui/ResultWidget.h
+++ src/gui/handler/ui/ResultWidget.h
@@ -11,12 +11,35 @@ namespace Ui {
 class ResultWidget;
 }
 
+/**
+ * @class ResultWidget
+ *
+ * @brief ResultWidget is a custom QWidget and is used to display the result page of the GUI.
+ *
+ * ResultWidget inherits QWidget and is therefore also a custom QWidget. It represents the result page of the GUI, that
+ * is, when a classification is finished and the user wants to see the classified image and the Top-5 probability to
+ * which labels the images match.
+ *
+ * It provides an interface for MainWindowHandler to abstract away the Qt objects from the rest of the software. The
+ * method displayResults() can be called with a ClassificationResult as parameter which will then initialize the user
+ * interface of ResultWidget with the needed data.
+ *
+ * @author Patrick Deubel
+ */
 class ResultWidget : public QWidget {
 
     Q_OBJECT
 
 public:
 
+    /**
+     * @brief The standard constructor initializes the ResultWidget
+     *
+     * It makes the QScrollArea resizable, which later holds the images and their (not aggregated) results and assings
+     * the image layout to its container.
+     *
+     * @param parent is an optional parent widget
+     */
     explicit ResultWidget(QWidget *parent = 0);
 
     ~ResultWidget();
