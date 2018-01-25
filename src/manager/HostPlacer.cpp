a2097dad0eb4debfe534bc8b849f5e297f3db12e
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-25 11:54:55 +0100

    add testcase for json-loader of ComputationHosts, change implementation to meet test requirements

--- src/manager/HostPlacer.cpp
+++ src/manager/HostPlacer.cpp
@@ -2,6 +2,7 @@
 // Created by jallmenroeder on 07/01/18.
 //
 
+#include <iostream>
 #include "HostPlacer.h"
 
 bool jsonComparator(const json& first, const json& second) {
@@ -10,17 +11,18 @@ bool jsonComparator(const json& first, const json& second) {
 
 
 HostPlacer::Performance HostPlacer::readComputationHostInfo(ComputationHost &c) {
-    std::string jsonStr = StringLoader::getStringFromFile("computationHosts.json");
-    json computationHost = json::parse(jsonStr)["computationHosts"];
+    std::string jsonStr = StringLoader::getStringFromFile("../../../src/manager/computationHosts.json");
+    json computationHostFile = json::parse(jsonStr);
+    json computationHost = computationHostFile["computationHosts"];
 
-    for (json jsonIt : computationHost) {
-        if (jsonIt["name"] == c.getName()) {
-            int power = jsonIt["power"];
-            int time = jsonIt["time"];
+    for (auto compHostIt : computationHost) {
+        if (compHostIt["name"] == c.getName()) {
+            int power = compHostIt["power"];
+            int time = compHostIt["time"];
             return Performance(power, time);
         }
     }
-
+    //TODO: real execption
     throw std::exception();
 }
 
--- tests/manager/HostPlacerTest.cpp
+++ tests/manager/HostPlacerTest.cpp
@@ -15,6 +15,16 @@ SCENARIO("Read values from JSON") {
     HostPlacer placer = HostPlacer();
 
     ComputationHost* localHost = new Executor("local");
+    ComputationHost* fpgaHost = new Executor("fpga");
+
+    REQUIRE(localHost->getName() == "local");
 
     HostPlacer::Performance localPerformance = placer.readComputationHostInfo(*localHost);
+    HostPlacer::Performance fpgaPerformance = placer.readComputationHostInfo(*fpgaHost);
+
+    REQUIRE(localPerformance.powerConsumption == 1500);
+    REQUIRE(localPerformance.timeConsumption == 500);
+
+    REQUIRE(fpgaPerformance.powerConsumption == 500);
+    REQUIRE(fpgaPerformance.timeConsumption == 1500);
 }
