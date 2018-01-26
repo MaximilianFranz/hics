36c93042a06a703400a52608c1c62ef637e275bf
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-26 23:40:55 +0100

    Implemented deleting and resetting of resultWidget and detailDialog when they are closed.

--- src/gui/handler/MainWindowHandler.cpp
+++ src/gui/handler/MainWindowHandler.cpp
@@ -13,11 +13,11 @@ MainWindowHandler::MainWindowHandler(std::list<NetInfo> &neuralNets, std::list<P
     mainWindow->addWidgetToStack(resultWidget);
 
     mainWindow->setCurrentWidget(startWidget);
-    //TODO initialze result and detail widget and add them to the stack
 
     connect(startWidget->getClassificationQPushButton(), SIGNAL(clicked()), this, SLOT(setClassificationRequestState()));
     connect(resultWidget->getReturnQPushButton(), SIGNAL(clicked()), this, SLOT(processReturnQPushButton()));
     connect(resultWidget->getDetailsQPushButton(), SIGNAL(clicked()), this, SLOT(processDetailQPushButton()));
+    connect(resultWidget, SIGNAL(destroyed()), this, SLOT(processReturnQPushButton())); //TODO Check if this works
 }
 
 void MainWindowHandler::setClassificationRequestState(){
@@ -51,12 +51,11 @@ void MainWindowHandler::processClassificationResult(const ClassificationResult &
 }
 
 void MainWindowHandler::processReturnQPushButton(){
-    //TODO do this also when resultWidget gets closed
-    //TODO reset detailDialog
     mainWindow->setCurrentWidget(startWidget);
-    ResultWidget* temp = resultWidget;
-    resultWidget = new ResultWidget();
-    delete temp;
+    delete resultWidget;
+    resultWidget = new ResultWidget(mainWindow);
+    delete detailDialog;
+    detailDialog = new DetailDialog(mainWindow);
 }
 
 void MainWindowHandler::processDetailQPushButton(){
