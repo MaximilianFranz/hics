c6acd2da5bcdccf83d246849d2801e311a68c370
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-26 23:45:32 +0100

    Added comments to MainWindowHandler.cpp

--- src/gui/handler/MainWindowHandler.cpp
+++ src/gui/handler/MainWindowHandler.cpp
@@ -4,19 +4,28 @@
 MainWindowHandler::MainWindowHandler(std::list<NetInfo> &neuralNets, std::list<PlatformInfo> &platforms,
                   std::list<OperationMode> &operationModes) {
 
+    //Initialize the used UI's
     mainWindow = new MainWindow();
     startWidget = new StartWidget(neuralNets, platforms, operationModes, mainWindow);
     resultWidget = new ResultWidget(mainWindow);
     detailDialog = new DetailDialog(mainWindow);
 
+    //mainWindow's QStackedWidget will be the main display
     mainWindow->addWidgetToStack(startWidget);
     mainWindow->addWidgetToStack(resultWidget);
 
     mainWindow->setCurrentWidget(startWidget);
 
+    //Starts the classification
     connect(startWidget->getClassificationQPushButton(), SIGNAL(clicked()), this, SLOT(setClassificationRequestState()));
+
+    //Deletes resultWidget
     connect(resultWidget->getReturnQPushButton(), SIGNAL(clicked()), this, SLOT(processReturnQPushButton()));
+
+    //Opens the detailDialog
     connect(resultWidget->getDetailsQPushButton(), SIGNAL(clicked()), this, SLOT(processDetailQPushButton()));
+
+    //Resets detailDialog and resultWidget when resultWidget is destroyed; display startWidget
     connect(resultWidget, SIGNAL(destroyed()), this, SLOT(processReturnQPushButton())); //TODO Check if this works
 }
 
@@ -30,6 +39,7 @@ void MainWindowHandler::setClassificationRequestState(){
     ClassificationRequest request(neuralNet, platforms, m, aggregate, userImgs);
     this->classificationRequestState = &request;
 
+    //Notify all observers that the state has changed
     notify();
 
     //TODO here maybe display loading screen/bar
@@ -45,8 +55,11 @@ ClassificationRequest* MainWindowHandler::getClassificationRequestState(){
 }
 
 void MainWindowHandler::processClassificationResult(const ClassificationResult &classificationResult){
+    //Initialize the results in resultWidget
     resultWidget->displayResults(classificationResult);
+    //Initialize the details in detailDialog
     detailDialog->insertDetails(&classificationResult);
+    //Change the currently displayed widget to resultWidget
     mainWindow->setCurrentWidget(resultWidget);
 }
 
