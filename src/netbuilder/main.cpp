ff89c186bfc5b940f583317b8347bb0b3b586da9
Max <franzmaximilian1996@gmail.com>
2018-01-27 15:14:18 +0100

    comment out legacy tests in main.cpp

--- src/netbuilder/main.cpp
+++ src/netbuilder/main.cpp
@@ -11,15 +11,15 @@ int main(void){
 
     AlexNetWeightLoader alex(FILE_PATH);
 
-    WeightWrapper conv1 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_1);
-    WeightWrapper conv2 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_2);
-    WeightWrapper conv3 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_3);
-    WeightWrapper conv4 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_4);
-    WeightWrapper conv5 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_5);
-    WeightWrapper dense1 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::FULLY_CON_1);
-    WeightWrapper dense2 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::FULLY_CON_2);
-    WeightWrapper dense3 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::FULLY_CON_3);
-
+//    WeightWrapper conv1 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_1);
+//    WeightWrapper conv2 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_2);
+//    WeightWrapper conv3 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_3);
+//    WeightWrapper conv4 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_4);
+//    WeightWrapper conv5 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::CONV_5);
+//    WeightWrapper dense1 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::FULLY_CON_1);
+//    WeightWrapper dense2 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::FULLY_CON_2);
+//    WeightWrapper dense3 = alex.getWeights(AlexNetWeightLoader::LayerIdentifier::FULLY_CON_3);
+//
 
 
 
