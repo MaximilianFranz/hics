a38829c013dc9e3b340f36f59d3b03b8e77c9145
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-27 15:00:13 +0100

    Created guitests CMakeList and added a test file for MainWindowSubject.

---
+++ tests/gui/MainWindowSubjectTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Patrick Deubel on 27.01.18.
+//
+
+#include "MainWindowSubjectTest.h"
---
+++ tests/gui/MainWindowSubjectTest.h
@@ -0,0 +1,7 @@
+//
+// Created by Patrick Deubel on 27.01.18.
+//
+
+#pragma once
+
+#include "
