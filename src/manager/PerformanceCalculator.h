007a018425e3f96f52bdfcda3aa9bf27e57cca8f
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-27 16:39:06 +0100

    change calculatePerformance to meet test requirements

--- src/manager/PerformanceCalculator.h
+++ src/manager/PerformanceCalculator.h
@@ -6,12 +6,22 @@
 
 
 #include "PerformanceData.h"
+#include <PlatformInfo.h>
+
+typedef std::vector<std::pair<PlatformInfo, float>> hostPlatformDistribution;
 
 /**
  * Calculates the performance of a classification according to the time each platform took to calculate.
  */
 class PerformanceCalculator {
 public:
+    struct HostInfo {
+        std::string jsonName;
+        float usage;
+        int time;
+
+        HostInfo(std::string name, float usage, int time) : jsonName(name), usage(usage), time(time) {}
+    };
 
     /**
      * Calculates the performance of a finished classification
@@ -19,5 +29,6 @@ public:
      * @param totalTime     total computation time
      * @return              total performance data
      */
-    static PerformanceData calculatePerformance(std::map<PlatformInfo, int> ptime, int totalTime);
+    static PerformanceData calculatePerformance(std::vector<hostPlatformDistribution> &platformDist,
+                                              std::vector<HostInfo> &hostDistribution);
 };
