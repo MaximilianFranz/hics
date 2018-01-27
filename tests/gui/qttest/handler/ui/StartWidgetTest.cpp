ffac074eae32a1f134fc22cc31e923d9c5d54a9a
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-27 17:10:45 +0100

    Splitted guitests in Qt test framework and catch framework.
    Since Qt offers a test framework to test its components and for example GUI specific interactions, I added a guitestsQt executable for this purpose and for other GUI tests a guitestsCatch executable.

---
+++ tests/gui/qttest/handler/ui/StartWidgetTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by pselab on 27.01.18.
+//
+
+#include "StartWidgetTest.h"
+
+void StartWidgetTest::test() {
+    int a = 3;
+    QVERIFY(a == 3);
+}
+
+QTEST_MAIN(StartWidgetTest)
---
+++ tests/gui/qttest/handler/ui/StartWidgetTest.h
@@ -0,0 +1,18 @@
+//
+// Created by pselab on 27.01.18.
+//
+
+#pragma once
+
+#include <QtTest/QtTest>
+
+class StartWidgetTest : public QObject
+{
+
+    Q_OBJECT
+
+private slots:
+
+    void test();
+};
+
