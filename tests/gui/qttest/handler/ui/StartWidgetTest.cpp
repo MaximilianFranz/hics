8a9ba5c841dcc6c8a08f39195467b9dfb272c759
Patrick Deubel <patrick.deubel@gmail.com>
2018-01-29 17:05:34 +0100

    Added test for adding images.

--- tests/gui/qttest/handler/ui/StartWidgetTest.cpp
+++ tests/gui/qttest/handler/ui/StartWidgetTest.cpp
@@ -14,13 +14,38 @@ void StartWidgetTest::classifyButtonClicked() {
     NetInfo net = request->getSelectedNeuralNet();
     QCOMPARE(net.getIdentifier(), nets.front().getIdentifier());
     QCOMPARE(request->getSelectedOperationMode(), modes.front());
+    //No platform selected
+    //TODO change this when error message implemented
+    QCOMPARE(request->getSelectedPlatforms().size(), (unsigned long)0);
+}
 
-    std::vector<PlatformInfo>::iterator itActual;
-    std::vector<PlatformInfo>::iterator itExpected;
- /*   for(itActual = request->getSelectedPlatforms().begin() && itExpected = platforms.begin(); itActual != request->getSelectedPlatforms().end() && itExpected != platforms.end(); ++itActual && ++itExpected){
-        QCOMPARE(itActual->getPlatformId(), )itActual
-    }*/
-    //QCOMPARE(request->getSelectedPlatforms(), platforms);
+void StartWidgetTest::addInputImage() {
+    startWidget->processInputImageButton();
+    //Select 3 images
+    QTest::mouseClick(startWidget->getClassificationQPushButton(), Qt::LeftButton);
+    ClassificationRequest* request = mainWindowHandler->getClassificationRequestState();
+    QCOMPARE(request->getUserImages().size(), (unsigned long)3);
+}
+
+void StartWidgetTest::changeOptions() {
+    NetInfo googlenet("GoogLeNet", 300, "googlenet");
+    PlatformInfo fgpa("FPGA", PlatformType::FPGA, "fpga", 5, 20);
+    PlatformInfo gpu("Titan XP", PlatformType::FPGA, "titanxp", 250, 100);
+    OperationMode mode2 = OperationMode::EnergyEfficient;
+    OperationMode mode3 = OperationMode::LowPower;
+
+    nets.push_back(googlenet);
+    platforms.push_back(fgpa);
+    platforms.push_back(gpu);
+    modes.push_back(mode2);
+    modes.push_back(mode3);
+
+    delete mainWindowHandler;
+    delete startWidget;
+    mainWindowHandler = new MainWindowHandler(nets, platforms, modes);
+    startWidget = mainWindowHandler->getStartWidget();
+
+    
 }
 
 QTEST_MAIN(StartWidgetTest)
