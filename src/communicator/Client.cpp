bece1d91eb25589c57f9dcd73d71e7125afce2d7
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-01-30 10:00:48 +0100

    implement classify via grpc

--- src/communicator/Client.cpp
+++ src/communicator/Client.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "Client.h"
+#include "Util.h"
+
+std::vector<ImageResult*> Client::classify(std::vector<ImageWrapper *> images, NetInfo net, OperationMode mode,
+                                           std::vector<PlatformInfo *> selectedPlatforms) {
+    ClassifyRequest request;
+    ClassifyReply reply;
+    ClientContext context;
+
+    for (ImageWrapper* img : images) {
+        //Create new ImageWrapperMessage to call classify via grpc
+        ImageWrapperMessage* imgMes = request.add_images();
+        Util::imageWrapperToMessage(img, imgMes);
+    }
+
+    //create and add netInfo message object
+    NetInfoMessage* netMes = request.mutable_net();
+    Util::netInfoToMessage(net, netMes);
+
+    //set operation mode
+    switch (mode) {
+        case OperationMode::EnergyEfficient     : request.set_mode(ClassifyRequest::EnergyEfficient);
+        case OperationMode::HighPower           : request.set_mode(ClassifyRequest::HighPower);
+        case OperationMode::LowPower            : request.set_mode(ClassifyRequest::LowPower);
+    }
+
+    //create and add all platforms
+    for (PlatformInfo* platform : selectedPlatforms) {
+        PlatformInfoMessage* platMes = request.add_selectedplatforms();
+        Util::platformInfoToMessage(platform, platMes);
+    }
+    //Client::stub_.operator*().classify(context, request, reply);
+    Status status = Client::stub_.operator*().classify(&context, request, &reply);
+
+    if (status.ok()) {
+        std::vector<ImageResult*> results;
+        for (int i = 0; i < reply.results_size(); i++) {
+            results.push_back(Util::messageToImageResult(&(reply.results(i))));
+        }
+        return results;
+    } else {
+        //TODO: specific excepion
+        throw std::exception();
+    }
+
+}
+
+std::vector<PlatformInfo*> Client::queryPlatform() {
+}
+
+std::vector<NetInfo*> Client::queryNets() {
+}
+
+//TODO: Cast methods from message objects to c++ and vice versa (see Util::)
--- src/communicator/Client.h
+++ src/communicator/Client.h
@@ -4,17 +4,33 @@
 
 #pragma once
 
+#include <grpc/grpc.h>
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_context.h>
+#include <grpc++/security/server_credentials.h>
+#include <grpc++/grpc++.h>
+#include <memory>
+#include <string>
 
 #include <ComputationHost.h>
 
-class Client : ComputationHost {
-//    Client();
-//
-//    std::vector<ImageResult> classify(std::vector<ImageWrapper> images, NetInfo net, OperationMode mode,
-//                                      std::vector<PlatformInfo> selectedPlatforms) override;
-//
-//    std::vector<PlatformInfo> queryPlatform() override;
-//
-//    std::vector<NetInfo> queryNets() override;
+#include "Messages/Communicator.grpc.pb.h"
+#include "Messages/Communicator.pb.h"
 
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+class Client : public ComputationHost {
+private:
+    std::unique_ptr<Communicator::Stub> stub_;
+public:
+    explicit Client(std::shared_ptr<Channel> channel) : stub_(Communicator::NewStub(channel)) {}
+
+    std::vector<ImageResult*> classify(std::vector<ImageWrapper*> images, NetInfo net, OperationMode mode,
+                                      std::vector<PlatformInfo*> selectedPlatforms) override;
+
+    std::vector<PlatformInfo*> queryPlatform() override;
+    std::vector<NetInfo*> queryNets() override;
 };
