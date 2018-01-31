107d792faa28947c0fbaa5a09057e3c4e565e589
Max <franzmaximilian1996@gmail.com>
2018-01-31 22:19:46 +0100

    Fixed PreProcessor to provide proper input data to the net.
    For some reason the x and y values in the QImage are changed and need to be inverted.
    Furthermore, normalization by substracting mean was added.

--- src/manager/PreProcessor.cpp
+++ src/manager/PreProcessor.cpp
@@ -26,73 +26,57 @@ std::vector<ImageWrapper> PreProcessor::processImages(std::map<QString, QImage> 
 
         //init Wrapper
         std::vector<int> dim = {3, height, width};
-        std::vector<float> bitmap;
+        std::vector<float> bitmap(3*height*width);
 
         double meanR = 0;
         double meanG = 0;
         double meanB = 0;
 
-        //create float bitmap, convention: {rgb, y, x} -- OUTPUT IS IN BGR format
-        for (int rgb = 2; rgb >= 0; rgb--) {
+        int index = 0;
+
+        //create float bitmap, convention: {rgb, y, x}
+        //-- OUTPUT IS IN BGR format -- X and Y are swapped for some reason!
+        for (int bgr = 0; bgr < 3; bgr++) {
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
-                    float currentPixel;
+                    float currentPixel = 0;
                     //Fill image with black pixels if dimension is wrong
-                    if (x >= currentImg.width() || y >= currentImg.height()) {
+                    if (y >= currentImg.width() || x >= currentImg.height()) {
                         currentPixel = 0;
-                        bitmap.push_back(currentPixel);
-                        continue;
                     }
+                    else {
                         //extract the right RGB channel
-                        switch (rgb) {
-                            case 0:
-                                //currentPixel = currentImg.pixelColor(x, y).red();
-                                currentPixel = qRed(currentImg.pixel(x, y));
-                                meanR += currentPixel / (height*width);
+                        switch (bgr) {
+                            case 2:
+                                currentPixel = qRed(currentImg.pixel(y, x));
+                                meanR += currentPixel / (height * width);
                                 break;
                             case 1:
-                                //currentPixel = currentImg.pixelColor(x, y).green();
-                                currentPixel = qGreen(currentImg.pixel(x, y));
-                                meanG += currentPixel / (height*width);
+                                currentPixel = qGreen(currentImg.pixel(y, x));
+                                meanG += currentPixel / (height * width);
                                 break;
-                            case 2:
-                                //currentPixel = currentImg.pixelColor(x, y).blue();
-                                currentPixel = qBlue(currentImg.pixel(x, y));
-                                meanB += currentPixel / (height*width);
+                            case 0:
+                                currentPixel = qBlue(currentImg.pixel(y, x));
+                                meanB += currentPixel / (height * width);
                                 break;
                             default:
                                 //TODO: specify exception
                                 throw std::exception();
                         }
-                    bitmap.push_back(currentPixel);
+                    }
+                    bitmap[index] = (currentPixel);
+                    index++;
                 }
             }
         }
 
-        std::cout << meanR << ":" << meanG << ":" << meanB << std::endl;
-
-        //substract mean
+        //substract mean to normalize
         for (int i = 0; i < bitmap.size(); i++) {
             bitmap[i] = float(bitmap[i] - (meanR + meanG + meanB)/3);
         }
 
-        /*for (int bgr = 0; bgr < 3; bgr++) {
-            for (int y = 0; y < height; y++) {
-                for (int x  = 0; x < width; x++) {
-                    switch (bgr) {
-                        case 0: bitmap[bgr + y *height + x * width] -= (meanB)*3;
-                            break;
-                        case 1: bitmap[bgr + y *height + x * width] -= (meanG)*3;
-                            break;
-                        case 2: bitmap[bgr + y *height + x * width] -= (meanR)*3;
-                            break;
-                        default: throw std::exception();
-                    }
-                }
-            }
-        }*/
-
         images.emplace_back(ImageWrapper(dim, bitmap, stdFilePath));
+
     }
 
     return images;
