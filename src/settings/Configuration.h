a1b6d1f5a8495881219c7c03ca392c43bb83dac2
Kateryna Prokopenko <udewb@student.kit.edu>
2018-02-01 10:57:44 +0100

    Documentation for the Configuration class.

--- src/settings/Configuration.h
+++ src/settings/Configuration.h
@@ -13,10 +13,25 @@ private:
 
 public:
 
+    /**
+     * Provides the path to the model json of the required neural net by its identifier.
+     * @param neuralNetIdentifier
+     * @return a path string to the net's model file
+     */
     std::string get_model_path(std::string neuralNetIdentifier);
 
+    /**
+     * Provides the path to the weights h5 file of the required neural net by its identifier.
+     * @param neuralNetIdentifier
+     * @return a path string to the net's weights file
+     */
     std::string get_weights_path(std::string neuralNetIdentifier);
 
+    /**
+     * Provides the path to the labels txt file of the required neural net by its identifier.
+     * @param neuralNetIdentifier
+     * @return a path string to the net's labels file
+     */
     std::string get_labels_path(std::string neuralNetIdentifier);
 
 };
