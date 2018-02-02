4e94beec89f33d2d8475e0c96e30ffa4770c5666
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-02-02 18:21:29 +0100

    broken commit to look over

---
+++ tests/communicator/UtilTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jallmenroeder on 02/02/18.
+//
+
+#include "UtilTest.h"
+#include "Util.h"
+#include "Communicator.grpc.pb.h"
+#include "Communicator.pb.h"
+
+SCENARIO("Test") {
+    PlatformInfoMessage* platMes = new PlatformInfoMessage();
+
+}
