31707db0a4fbbfb6da994155cf4fca57f9ebcc4e
Patrick Deubel <patrick.deubel@gmail.com>
2018-02-07 20:10:55 +0100

    Added coloring of result labels according to the percentage the label has.

--- src/gui/handler/ui/ResultWidget.cpp
+++ src/gui/handler/ui/ResultWidget.cpp
@@ -83,7 +83,7 @@ void ResultWidget::displayResults(ClassificationResult *classificationResult) {
 QFrame *ResultWidget::createImageLayout(const std::string &filePath) {
     QFrame* frame = new QFrame(this);
     frame->setFrameShape(QFrame::Box);
-    
+
     QVBoxLayout *imageLayout = new QVBoxLayout(frame);
     imageLayout->insertStretch(0);
     //Displays the file path
@@ -148,6 +148,11 @@ QFrame *ResultWidget::createResultLayout(std::vector<std::pair<std::string, floa
         //TODO when percentage too long round the number
         percentage->setText(QString::number(pair.second*100) + "%");
         percentage->setAlignment(Qt::AlignRight);
+        percentage->setStyleSheet("background: qlineargradient"
+                                      "(x1:0, y1:0, x2:1, y2:0,"
+                                      " stop:0 rgba(0, 0, 0, 0),"
+                                      " stop:" + QString::number(1-pair.second) +" rgba(0, 0, 0, 0),"
+                                      " stop:1 rgba(255, 0, 0, 0.6))");
         labelLayout->addWidget(percentage);
 
         layout->addLayout(labelLayout);
