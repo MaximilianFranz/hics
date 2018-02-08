2db42cbab850031be55786e901dd250761357fc1
Michael Biebl <mbiebl@gmail.com>
2018-02-08 12:10:44 +0100

    Disable debug window in gui

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -4,94 +4,13 @@
 
 #include <QApplication>
 #include <Manager.h>
-#include "handler/ui/MainWindow.h"
-#include "handler/MainWindowHandler.h"
-#include "PerformanceData.h"
-#include "ClassificationResult.h"
-#include "handler/ui/DetailDialog.h"
-#include <QStackedLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
 
-        ResultWidget* resultWidget = new ResultWidget();
-////
-        NetInfo net("AlexNet", 227, "alexnet");
-////    PlatformInfo platform("CPU", PlatformType::CPU, "id", 100, 4);
-////    OperationMode mode(HighPower);
-////    OperationMode mode2(LowPower);
-////    OperationMode mode3(EnergyEfficient);
-////
-////
-////    std::list<NetInfo> nets;
-////    nets.push_back(net);
-////    std::list<PlatformInfo> plats;
-////    plats.push_back(platform);
-////    std::list<OperationMode> modes;
-////    modes.push_back(mode);
-////    modes.push_back(mode2);
-////    modes.push_back(mode3);
-////
-////
-////    MainWindowHandler handler(nets, plats, modes);
-//
-//
-    std::vector<std::pair<std::string, float>> results;
-    std::pair<std::string, float> pair1("Leopard, LeopardLeopardLeopardLeopardLeopardLeopardLeopardLeopardLeopardLeopardLeopard", 0.0029);
-    std::pair<std::string, float> pair2("Haus", 0.17);
-    std::pair<std::string, float> pair3("Tiger", 0.09);
-    std::pair<std::string, float> pair4("KIT", 0.016);
-    std::pair<std::string, float> pair5("Baukran", 0.684);
-    results.push_back(pair1);
-    results.push_back(pair2);
-    results.push_back(pair3);
-    results.push_back(pair4);
-    results.push_back(pair5);
-
-    std::vector<int> b(3);
-    ImageWrapper imageWrapper(b, "/home/pselab/Bilder/landscape-1843128_960_720.jpg");
-    std::vector<std::pair<PlatformInfo*, float>> plat;
-    PlatformInfo* info1 = new PlatformInfo("CPU", PlatformType::CPU, "cpu", 100, 4);
-    PlatformInfo* info2 = new PlatformInfo("FPGA1", PlatformType::FPGA, "fpga1", 50, 3);
-    PlatformInfo* info3 = new PlatformInfo("GPU1", PlatformType::GPU, "gpu1", 34, 55);
-    PlatformInfo* info4 = new PlatformInfo("GPU2", PlatformType::GPU, "gpu2", 99, 211);
-
-    plat.push_back(std::pair<PlatformInfo*, float>(info1, 0.20));
-    plat.push_back(std::pair<PlatformInfo*, float>(info2, 0.10));
-    plat.push_back(std::pair<PlatformInfo*, float>(info3, 0.1));
-    plat.push_back(std::pair<PlatformInfo*, float>(info4, 0.69));
-
-    PerformanceData performanceData(15, 999, plat);
-    ImageResult imgResult1(results, imageWrapper);
-    ImageResult imgResult2(results, imageWrapper);
-    ImageResult imgResult3(results, imageWrapper);
-    ImageResult imgResult4(results, imageWrapper);
-    ImageResult imgResult5(results, imageWrapper);
-
-    std::vector<ImageResult> imgResults;
-    imgResults.push_back(imgResult1);
-    imgResults.push_back(imgResult2);
-    imgResults.push_back(imgResult3);
-    imgResults.push_back(imgResult4);
-    imgResults.push_back(imgResult5);
-
-    ClassificationResult* classificationResult = new ClassificationResult(imgResults, net, performanceData);
-    //classificationResult->aggregateResults();
-    resultWidget->displayResults(classificationResult);
-    resultWidget->show();
-//
-//
-//
-////    delete resultWidget;
-//
-//    DetailDialog* d = new DetailDialog();
-//    d->insertDetails(classificationResult);
-//    d->show();
-////
-//
-  Manager manager;
+   Manager manager;
    manager.initGUI();
 
 
