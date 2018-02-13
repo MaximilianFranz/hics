5c2fb3a75e8ff380c02bfa62fa06127cbca7a33e
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-02-13 18:27:58 +0100

    change static class Util to namespace

--- src/communicator/Util.h
+++ src/communicator/Util.h
@@ -13,14 +13,13 @@
 #include "Communicator.pb.h"
 
 
-class Util {
-public:
-    static void imageWrapperToMessage(const ImageWrapper* img, ImageWrapperMessage *messagePtr);
-    static void netInfoToMessage(NetInfo *net, NetInfoMessage *messagePtr);
-    static void platformInfoToMessage(const PlatformInfo* platform, PlatformInfoMessage* messagePtr);
-    static void imageResultToMessage(const ImageResult* result, ImageResultMessage* resultPtr);
-    static ImageWrapper* messageToImageWrapper(const ImageWrapperMessage* imgMes);
-    static PlatformInfo* messageToPlatformInfo(const PlatformInfoMessage* platMes);
-    static ImageResult* messageToImageResult(const ImageResultMessage* imgMes);
-    static NetInfo* messageToNetInfo(const NetInfoMessage* net);
+namespace Util {
+    void imageWrapperToMessage(const ImageWrapper* img, ImageWrapperMessage *messagePtr);
+    void netInfoToMessage(NetInfo *net, NetInfoMessage *messagePtr);
+    void platformInfoToMessage(const PlatformInfo* platform, PlatformInfoMessage* messagePtr);
+    void imageResultToMessage(const ImageResult* result, ImageResultMessage* resultPtr);
+    ImageWrapper* messageToImageWrapper(const ImageWrapperMessage* imgMes);
+    PlatformInfo* messageToPlatformInfo(const PlatformInfoMessage* platMes);
+    ImageResult* messageToImageResult(const ImageResultMessage* imgMes);
+    NetInfo* messageToNetInfo(const NetInfoMessage* net);
 };
