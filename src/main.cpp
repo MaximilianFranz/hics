2e40e7e34fca5a5257b88c20ef9bdc946eb13e31
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-02-15 15:54:42 +0100

    add main

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Patrick Deubel on 08.01.18.
+//
+
+#include <QApplication>
+#include <Manager.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    Manager manager;
+    manager.initGUI();
+    return a.exec();
+}
