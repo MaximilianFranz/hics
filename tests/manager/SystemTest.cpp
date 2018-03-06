4ef1394db729309d5609437e2e0ba8ab96dbb4d9
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-03-06 23:03:18 +0100

    add gui interaction system test

--- tests/manager/SystemTest.cpp
+++ tests/manager/SystemTest.cpp
@@ -2,5 +2,18 @@
 // Created by jallmenroeder on 06/03/18.
 //
 
+#include <Manager.h>
+#include <QtWidgets/QApplication>
 #include "SystemTest.h"
 
+SCENARIO("System Test") {
+    SECTION("Complete Test") {
+        char *argv[] = {"program name", "arg1", "arg2", NULL};
+        int argc = sizeof(argv) / sizeof(char*) - 1;
+        QApplication a(argc, argv);
+
+        Manager manager;
+        manager.initGUI();
+        a.exec();
+    }
+}
