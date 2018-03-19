107ee8b6567f1f20ac975121384d8c5f84fddd62
Jan Allmenroeder <jallmenroeder@gmail.com>
2018-03-19 23:10:02 +0100

    add tests to simulate aggregate results bug

--- tests/manager/ClassificationResultTest.cpp
+++ tests/manager/ClassificationResultTest.cpp
@@ -39,17 +39,21 @@ SCENARIO("aggregateResult for trivial test case") {
 
     PerformanceData perfData = PerformanceData(1000, 20, usage);
 
-    std::vector<int> dim1 = {256, 256, 3};
-    ImageWrapper img1 = ImageWrapper(dim1, "PathToFile");
+    std::vector<int> dim = {256, 256, 3};
+    ImageWrapper img1 = ImageWrapper(dim, "PathToFile");
     std::vector<label> labels1 = {label("car", 0.7), label("bike", 0.2), label("house", 0.05), label("bird", 0.025),
                                  label("cat", 0.025)};
     ImageResult imgRes1 = ImageResult(labels1, img1);
 
-    std::vector<int> dim2 = {256, 256, 3};
-    ImageWrapper img2 = ImageWrapper(dim2, "PathToFile");
+    ImageWrapper img2 = ImageWrapper(dim, "PathToFile");
     std::vector<label> labels2 = {label("car", 0.8), label("bike", 0.1), label("house", 0.09), label("dog", 0.005),
                                   label("cat", 0.005)};
     ImageResult imgRes2 = ImageResult(labels2, img2);
+
+    ImageWrapper img3 = ImageWrapper(dim, "PathToFile");
+    std::vector<label> labels3 = {label("l1", 0.4), label("l2", 0.04), label("l3", 0.01), label("l4", 0.005),
+                                  label("l5", 0.0001)};
+    ImageResult imgRes3 = ImageResult(labels3, img3);
 
 
 
@@ -104,6 +108,33 @@ SCENARIO("aggregateResult for trivial test case") {
         labelIt++;
         REQUIRE(labelIt.operator*().first == "bird");
         REQUIRE(std::abs(labelIt.operator*().second - 0.0125f) < eps);
+    }
+
+    SECTION("Test trimming of results") {
+        std::vector<ImageResult> results = {imgRes3, imgRes1};
+
+        ClassificationResult clasRes = ClassificationResult(results, net, perfData);
+        clasRes.aggregateResults();
+
+        std::vector<std::pair<std::string, float>> aggLabels = clasRes.getAggregatedResults();
+
+        REQUIRE(aggLabels.size() == 5);
+        auto labelIt = aggLabels.begin();
+
+        REQUIRE(labelIt.operator*().first == "car");
+        REQUIRE((labelIt.operator*().second - 0.35f) < eps);
+        labelIt++;
+        REQUIRE(labelIt.operator*().first == "l1");
+        REQUIRE((labelIt.operator*().second - 0.2f) < eps);
+        labelIt++;
+        REQUIRE(labelIt.operator*().first == "bike");
+        REQUIRE((labelIt.operator*().second - 0.1f) < eps);
+        labelIt++;
+        REQUIRE(labelIt.operator*().first == "house");
+        REQUIRE((labelIt.operator*().second - 0.025f) < eps);
+        labelIt++;
+        REQUIRE(labelIt.operator*().first == "l2");
+        REQUIRE((labelIt.operator*().second - 0.02f) < eps);
     }
 
     SECTION("Test neural net getter") {
